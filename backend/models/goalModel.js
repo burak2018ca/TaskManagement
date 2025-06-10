0cb826021c05feae4c5c7a5abf993a6386cdee4c
Burak_Yildirim <burak2018ca@hotmail.com>
2025-06-10 16:11:07 -0400

    Initial REST API for goals

---
+++ backend/models/goalModel.js
@@ -0,0 +1,13 @@
+const { text } = require('body-parser')
+const { time, timeStamp } = require('console')
+const mongoose = require('mongoose')
+const { type } = require('os')
+
+const goalSchema =  mongoose.Schema({
+    text:{
+        type: String,
+        required: [true, 'Please add a text value'],
+    } }, {timeStamps: true}
+)
+
+module.exports = mongoose.model('Goal', goalSchema)
