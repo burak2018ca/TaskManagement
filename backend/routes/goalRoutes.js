fd626d28dfd57b3948caa7896e4d651dc524e6ec
Burak_Yildirim <burak2018ca@hotmail.com>
2025-06-10 14:35:23 -0400

    first commit

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controllers/goalController')
+
+router.route('/').get(getGoals).post(setGoal)
+// router.get('/', getGoals)
+// router.post('/', setGoal)
+
+
+router.route('/:id').delete(deleteGoal).put(updateGoal)
+// router.put('/:id', addGoal)
+// router.delete('/:id', deleteGoal)
+
+
+module.exports =  router
