0b551dfb234f5d21e11caae235b89cffbb28d475
Burak_Yildirim <burak2018ca@hotmail.com>
2025-06-16 12:14:10 -0400

    Finished the backend API for Register and Login User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,30 +1,88 @@
-// const expressAsyncHandler = require("express-async-handler");
-// const User = require('../models/userModel');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
 
 // @desc Register New User
 // @route Post /api/users
 // @access Public
-const registerUser = (req, res) => {
-    res.json({message: 'Register User'})
-}
+const registerUser = asyncHandler( async(req, res) => {
+    const { name, email, password } = req.body
+
+    if(!name || !email || !password){
+        res.status(400);
+        throw new Error('Please add all fields')
+    }
+
+    //Check if user exists
+    const userExists = await User.findOne({email})
+    if(userExists){
+        res.status(400)
+        throw new Error('This user already exists')
+    }
+
+    //Hash the password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //Create a user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+
+    //Check and Confirm if user created
+    if(user){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email:user.email,
+            token: generateToken(user._id)
+        })
+    }
+    else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
 
 
 // @desc Auth User
 // @route Post /api/users/login
 // @access Public
-const loginUser = (req, res) => {
-    res.json({message: 'Login User'})
-}
+const loginUser = asyncHandler( async(req, res) => {
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+    
+    if(user && await(bcrypt.compare(password, user.password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email:user.email,
+            token: generateToken(user._id)
+        })
+    }
+    else{
+        res.status(400)
+        throw new Error('Invalid login info')
+    }
+})
 
 
 
 // @desc Get User Data
 // @route GET /api/users/me
 // @access Public
-const getMe = (req, res) => {
+const getMe = asyncHandler( async(req, res) => {
     res.json({message: 'User data display'})
-}
+})
 
+
+// Generate a Token
+const generateToken = (id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET, {expiresIn: '30d'})
+}
 
 module.exports = {
     registerUser,
